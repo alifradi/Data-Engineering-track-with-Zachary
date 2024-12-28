a8671b37f792ef424a86337a04cd21701ca7808b
alifradi <alifrai7@gmail.com>
2024-12-28 18:38:51 -0500

    Assignment 1 is done !

--- Dimensional Data Modeling.sql
+++ Dimensional Data Modeling.sql
@@ -99,13 +99,14 @@ select * from actor_films where actor ='Al Pacino'
 -- Implements type 2 dimension modeling (i.e., includes start_date and end_date fields).
 -- Tracks quality_class and is_active status for each actor in the actors table.
 drop table actors_history_scd
-create table actors_history_scd  (
+create table actors_history_scd(
 actorid TEXT,
 actor TEXT,
 is_active boolean,
 quality_class quality_class,
 start_date integer,
-end_date integer
+end_date integer,
+current_season integer
 );
 
 -- 4. Backfill query for actors_history_scd: Write a "backfill" query that can populate the entire actors_history_scd table in a single query. 
@@ -144,10 +145,119 @@ SELECT
   is_active,
   quality_class,
   MIN(production_year) AS start_date,
-  MAX(production_year) AS end_date
+  MAX(production_year) AS end_date,
+  2020 as current_season
 FROM with_streaks
 GROUP BY actorid, actor, is_active, quality_class, streak_identifier
 order by actor, start_date
 
 -- CHECK the entire fill
 select * from actors_history_scd
+
+-- 5. Incremental query for actors_history_scd: Write an "incremental" query that combines the previous year's SCD data with new incoming data from the actors table.
+-- we're in 2021 and we want to update SCD table to track changements using the incoming data of 2021
+-- Drop and recreate the custom type for SCD tracking
+DROP TYPE IF EXISTS scd_type;
+
+CREATE TYPE scd_type AS (
+    is_active BOOLEAN,
+    quality_class QUALITY_CLASS,
+    start_date INTEGER,
+    end_date INTEGER
+);
+
+WITH 
+changes_history_archive AS (
+    -- Records with changements stopped before 2020 won't change anymore
+    SELECT 
+        actorid,
+        actor,
+        is_active,
+        quality_class,
+        start_date,
+        end_date
+    FROM actors_history_scd
+    WHERE current_season = 2020 AND end_date < 2020
+),
+last_year_scd AS (
+    -- Records SCD for last year that are up to change or stop changing
+    SELECT 
+        actorid,
+        actor,
+        is_active,
+        quality_class,
+        start_date,
+        end_date
+    FROM actors_history_scd
+    WHERE current_season = 2020 AND end_date = 2020
+),
+this_season_data AS (
+    -- Selecting data processed of current year
+    SELECT * 
+    FROM actors
+    WHERE year = 2021
+), 
+unchanged_records AS (
+    -- Fetch and update actors SCD records that didn't change from last to current year but are active before
+    SELECT 
+        ts.actorid,
+        ts.actor,
+        ts.is_active,
+        ts.quality_class,
+        ls.start_date,
+        ts.year AS end_date
+    FROM this_season_data ts
+    JOIN last_year_scd ls
+        ON ls.actorid = ts.actorid
+    WHERE ts.quality_class = ls.quality_class 
+      AND ts.is_active = ls.is_active
+), 
+changed_records AS (
+    -- Get 2 records per actor: one from last and the other from this season, showing changements within tracked columns
+    SELECT 
+        ts.actorid,
+        ts.actor,
+        unnest(array[
+            ROW(ls.is_active, ls.quality_class, ls.start_date, ls.end_date)::scd_type,
+            ROW(ts.is_active, ts.quality_class, ts.year, ts.year)::scd_type
+        ]) AS records
+    FROM this_season_data ts
+    LEFT JOIN last_year_scd ls
+        ON ls.actorid = ts.actorid
+    WHERE ts.quality_class <> ls.quality_class 
+       OR ts.is_active <> ls.is_active
+), 
+unnested_changed_records AS (
+    -- Reformat the old and the new records that have changements from last season to today's data
+    SELECT
+        actorid, 
+        actor,
+        (records).is_active AS is_active,
+        (records).quality_class AS quality_class,
+        (records).start_date AS start_date,
+        (records).end_date AS end_date
+    FROM changed_records
+),
+new_records AS (
+    -- Actors that weren't active till today's date
+    SELECT 
+        ts.actorid,
+        ts.actor,
+        ts.is_active,
+        ts.quality_class,
+        ts.year AS start_date,
+        ts.year AS end_date
+    FROM this_season_data ts
+    LEFT JOIN last_year_scd ls
+        ON ts.actorid = ls.actorid
+    WHERE ls.actor IS NULL
+)
+-- Combine all SCD data: history, unchanged, changed, and new records
+SELECT * FROM changes_history_archive
+UNION ALL
+SELECT * FROM unnested_changed_records
+UNION ALL
+SELECT * FROM new_records
+UNION ALL
+SELECT * FROM unchanged_records
+ORDER BY actor, start_date;
