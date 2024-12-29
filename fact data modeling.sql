c8ed9b27fdd7b7e8c16a82eeb5a3d624aa453790
Fradi Ali <38408668+alifradi@users.noreply.github.com>
2024-12-29 13:07:25 -0500

    Update fact data modeling.sql

--- fact data modeling.sql
+++ fact data modeling.sql
@@ -12,3 +12,18 @@ FROM (
     FROM game_details
 ) AS games_deduped
 WHERE row_num = 1; -- 18768 rows 96% less data
+
+-- Exploring data
+select * from events -- url,referrer, user_id, device_id, host, event_time
+select distinct host from events -- 3 values
+select distinct device_id from events -- 349
+select distinct event_time from events --8415
+select distinct user_id from events -- 1432
+select distinct url from events -- 382
+select distinct referrer from events -- 163
+
+select * from devices
+-- print column names from devices
+SELECT column_name 
+FROM INFORMATION_SCHEMA.COLUMNS 
+WHERE table_name = 'devices'; -- device_id,browser_version_patch,device_version_minor,device_version_patch,os_version_minor,os_version_patch,browser_version_major,browser_version_minor,browser_type,device_version_major,os_type,os_version_major, device_type
