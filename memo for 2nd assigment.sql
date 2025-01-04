9520bc3cdf62134ae50f62de5e569a017dc65dfc
Fradi Ali <38408668+alifradi@users.noreply.github.com>
2025-01-04 13:51:49 -0500

    Update memo for 2nd assigment.sql

--- memo for 2nd assigment.sql
+++ memo for 2nd assigment.sql
@@ -1,3 +1,20 @@
+with deduped as (
+select 
+g.game_date_est,
+gd.*, 
+row_number() over (partition by player_id, gd.game_id, team_id order by g.game_date_est) as row_num 
+from game_details gd
+join games g on gd.game_id = g.game_id
+)
+select * 
+from deduped 
+where row_num =1 
+
+
+select* from game_details
+
+
+
 select 
 min(event_time),
 max(event_time)
@@ -90,9 +107,83 @@ CROSS JOIN series
 select user_id,
 sum(place_holder_int)::bigint::bit(32) as history_active,
 bit_count(sum(place_holder_int)::bigint::bit(32)) as days_active,
-bit_count('10000000000000000000000000000000'::bit(32)& sum(place_holder_int)::bigint::bit(32) )>0 as weekly_active,
-bit_count('11111110000000000000000000000000'::bit(32)& sum(place_holder_int)::bigint::bit(32) )>0 as weekly_active,
+bit_count('10000000000000000000000000000000'::bit(32)& sum(place_holder_int)::bigint::bit(32) )>0 as last_day_active,
+bit_count('11111110000000000000000000000000'::bit(32)& sum(place_holder_int)::bigint::bit(32) )>0 as last_week_active,
 bit_count(sum(place_holder_int)::bigint::bit(32) )>0 as daily_active
 from place_holder_in
 group by user_id
 
+
+
+
+create table array_metrics (
+user_id numeric,
+month_start date,
+metric_name text,
+metric_array real[]
+,primary key (user_id, month_start, metric_name)
+)
+
+delete from array_metrics
+-- Insert aggregated metrics into the array_metrics table
+INSERT INTO array_metrics
+WITH daily_aggregate AS (
+    -- Aggregate daily site hits per user
+    SELECT 
+        user_id,
+        DATE(event_time) AS date,
+        COUNT(1) AS num_site_hits
+    FROM events
+    WHERE DATE(event_time) = DATE('2023-01-03')
+    AND user_id IS NOT NULL
+    GROUP BY user_id, DATE(event_time)
+),
+yesterday_array AS (
+    -- Retrieve existing metrics for the month starting from '2023-01-01'
+    SELECT *
+    FROM array_metrics 
+    WHERE month_start = DATE('2023-01-01')
+)
+SELECT 
+    -- Select user_id from either daily_aggregate or yesterday_array
+    COALESCE( da.user_id, ya.user_id) AS user_id,
+    -- Determine month_start date
+    COALESCE(ya.month_start, DATE_TRUNC('month', da.date)) AS month_start,
+    -- Set metric name to 'site_hits'
+    'site_hits' AS metric_name,
+    -- Update metric_array based on existing data and new daily aggregates
+    CASE 
+        WHEN ya.metric_array IS NOT NULL THEN 
+            ya.metric_array || ARRAY[COALESCE(da.num_site_hits,0)] 
+        WHEN ya.metric_array IS NULL THEN
+            ARRAY_FILL(0, ARRAY[COALESCE (date - DATE(DATE_TRUNC('month', date)), 0)]) 
+                || ARRAY[COALESCE(da.num_site_hits,0)]
+    END AS metric_array
+FROM daily_aggregate da
+FULL OUTER JOIN yesterday_array ya 
+ON da.user_id = ya.user_id
+ON CONFLICT (user_id, month_start, metric_name)
+DO 
+    UPDATE SET metric_array = EXCLUDED.metric_array;
+
+
+select * from array_metrics
+
+-- Uncomment and run the following query to verify the cardinality of metric_array
+-- SELECT cardinality(metric_array), COUNT(1)
+-- FROM array_metrics
+-- GROUP BY 1;
+
+-- Aggregate metrics by summing specific elements in the metric_array
+WITH agg AS (
+    SELECT metric_name, month_start, ARRAY[SUM(metric_array[1]), SUM(metric_array[2]), SUM(metric_array[3])] AS summed_array
+    FROM array_metrics
+    GROUP BY metric_name, month_start
+)
+-- Select and display the metric_name, date (adjusted by index), and summed value
+SELECT 
+    metric_name, 
+    month_start + CAST(CAST(index - 1 AS TEXT) || ' day' AS INTERVAL) AS adjusted_date,
+    elem AS value
+FROM agg
+CROSS JOIN UNNEST(agg.summed_array) WITH ORDINALITY AS a(elem, index);
